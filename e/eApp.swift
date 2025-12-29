a039f535c13dd3b3d3f92d442fc167c30874ac20
sweng07 <stevenweng47@gmail.com>
2025-12-29 14:52:50 -0500

    Initial Commit

---
+++ e/eApp.swift
@@ -0,0 +1,32 @@
+//
+//  eApp.swift
+//  e
+//
+//  Created by Steven Weng on 2025-12-29.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct eApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ eTests/eTests.swift
@@ -0,0 +1,17 @@
+//
+//  eTests.swift
+//  eTests
+//
+//  Created by Steven Weng on 2025-12-29.
+//
+
+import Testing
+@testable import e
+
+struct eTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
